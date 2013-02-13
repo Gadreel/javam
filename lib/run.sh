8d676454bf16e6c8f851c0ef9f403d1378994870
Gadreel <andy@etimeline.com>
2013-02-13 13:39:53 +0000

    adding run script

---
+++ lib/run.sh
@@ -0,0 +1,14 @@
+export gtm_chset="UTF-8"
+export gtm_noundef="TRUE"
+export TZ="GTM"
+
+source /usr/local/gtm/gtmprofile
+
+#export gtm_dist=/usr/local/gtm
+export GTMCI=${HOME}/javam/gtm_ci_old/calltab.ci
+export gtmroutines="${HOME}/javam/gtm_ci_old ${gtmroutines}"
+export LD_LIBRARY_PATH=${gtm_dist}:${LD_LIBRARY_PATH}
+
+java -Djava.library.path=. -jar JavaM.jar $1 $2 $3 $4 $5 $6 $7
+
+stty sane
